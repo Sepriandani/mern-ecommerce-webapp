6daa6ae3d6514cbf945a85188acbb1397a4f1470
sepriandani <sepriandanni@gmail.com>
2022-08-04 07:20:24 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,94 @@
+import ImgSllide1 from './img/slide1.jpg'
+import ImgSllide2 from './img/slide2.jpg'
+import ImgSllide3 from './img/slide3.jpg'
+import ImgCategories1 from './img/tshirt.jpg'
+import ImgCategories2 from './img/jeans.jpg'
+import ImgCategories3 from './img/hoodie.jpg'
+import ImgPopular1 from './img/popular1.png'
+import ImgPopular2 from './img/popular2.png'
+import ImgPopular3 from './img/popular3.png'
+import ImgPopular4 from './img/popular4.png'
+import ImgPopular5 from './img/popular5.png'
+import ImgPopular6 from './img/popular6.png'
+import ImgPopular7 from './img/popular7.png'
+import ImgPopular8 from './img/popular8.png'
+
+export const  sliderItems = [
+    {
+        id: 1,
+        img: ImgSllide1,
+        title: "SUMMER SALE",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: ImgSllide2,
+        title: "WINTER SALE",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        bg: "fafafc",
+    },
+    {
+        id: 3,
+        img: ImgSllide3,
+        title: "POPULAR SALE",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        bg: "fdfafe",
+    },
+
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: ImgCategories1,
+        title: "SHIRT",
+    },
+    {
+        id: 2,
+        img: ImgCategories2,
+        title: "JEANS",
+    },
+    {
+        id: 3,
+        img: ImgCategories3,
+        title: "HOODIE",
+    },
+
+];
+
+export const popularProduct = [
+    {
+        id: 1,
+        img: ImgPopular1,
+    },
+    {
+        id: 2,
+        img: ImgPopular2,
+    },
+    {
+        id: 3,
+        img: ImgPopular3,
+    },
+    {
+        id: 4,
+        img: ImgPopular4,
+    },
+    {
+        id: 5,
+        img: ImgPopular5,
+    },
+    {
+        id: 6,
+        img: ImgPopular6,
+    },
+    {
+        id: 7,
+        img: ImgPopular7,
+    },
+    {
+        id: 8,
+        img: ImgPopular8,
+    },
+
+];
